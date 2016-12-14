88aa53d60a201b3944a75e6b34c12bca8aa042af
linchenhui <linchenhui@qq.com>
2016-12-14 10:57:57 +0800

    删除使用代码

--- app/controllers/Index.php
+++ app/controllers/Index.php
@@ -10,30 +10,7 @@ class Index extends Controller
 
 	function index()
 	{
-//        echo 1;die;
-//		$client = new \Yar_Client("http://lch/index/yar");
-//		$result = $client->api("parameter");
-//		var_dump($result);
-//		\Yar_Concurrent_Client::call("http://lch/index/yar", "app\\models\\Test", array("one"),"app\\controllers\\Index::callback");
-		\Yar_Concurrent_Client::call("http://lch/index/yar", "api", array("parameters"), "app\\controllers\\Index::callback");
-//		\Yar_Concurrent_Client::call("http://lch/index/yar", "Test", array("parameters"), "callback");
-//		\Yar_Concurrent_Client::call("http://lch/index/yar", "Test", array("parameters"), "callback");
-		\Yar_Concurrent_Client::loop();
-//		$service = new \Yar_Server(new Test());
-//		$service->handle();
-//		phpinfo();
-//		$this->display('index/index');
-	}
-	static function callback($retval, $callinfo) {
-		var_dump($retval);
-	}
-	public function api($parameter, $option = "foo") {
-
-		return $parameter;//var_dump($parameter);
-	}
-	public function yar(){
-		$service = new \Yar_Server(new Test());
-		$service->handle();
+        echo 1;die;
 	}
 
 }
